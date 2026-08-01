cd70fa346c792a6aa27a54c5f3d04a6194cc42fa
Vladislav Zabilskij <zabilskijvladislav@gmail.com>
2026-08-01 17:49:33 +0300

    2. aiogram-bot echo

---
+++ bot.py
@@ -0,0 +1,30 @@
+# Standard
+import os
+import asyncio
+# Special
+from dotenv import load_dotenv
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
+# Local
+
+
+# Loading variables from a dotenv file
+load_dotenv()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+# Creating a dispatcher
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def command_start_handler(message: Message) -> None:
+    await message.answer("Hello, World!")
+
+
+# Run the bot
+async def main() -> None:
+    bot = Bot(token=BOT_TOKEN)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
