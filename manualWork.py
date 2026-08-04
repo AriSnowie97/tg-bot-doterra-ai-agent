968a377a2e88a57f60cd9ea060bcec9c927dd31b
Vladislav Zabilskij <zabilskijvladislav@gmail.com>
2026-08-04 19:07:45 +0300

    Fix path

--- manualWork.py
+++ manualWork.py
@@ -7,7 +7,7 @@ from src.storage import storage
 
 def main():
     storage.creating_table_for_chunks()
-    storage.upsert_file_of_chunks(Path("src\\content\\docs\\frankincense_chunks.json"))
+    storage.upsert_file_of_chunks(Path("src/content/docs/frankincense_chunks.json"))
 
 
 if __name__ == "__main__":
