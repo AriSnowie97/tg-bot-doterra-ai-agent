a6223d8ce9b565225b3ec66a6db8ee41d89e8ff2
AriSnowie97 <diysylveon@gmail.com>
2026-08-18 11:47:52 +0300

    fix: make Article Link block-level, darken text-muted in light theme

--- mini_app_web/src/components/pages/Articles/components/Article/Article.jsx
+++ mini_app_web/src/components/pages/Articles/components/Article/Article.jsx
@@ -5,7 +5,10 @@ import styles from "./Article.module.css";
 
 const Article = ({data}) => {
     return (
-        <Link to={`/article/${data.slug || 'home-essentials-enrollment-kit'}`} style={{textDecoration: 'none'}}>
+        <Link 
+            to={`/article/${data.slug || 'home-essentials-enrollment-kit'}`} 
+            style={{ textDecoration: 'none', display: 'block', width: '100%', boxSizing: 'border-box' }}
+        >
             <div className={styles.wrapper}>
                 <div className={styles.img}>Prod Img</div>
                 <div className={styles.description}>
